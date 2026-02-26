4958c01a103380224e98c63c5df8cf2dc1284843
deku99hh <omar.rashed1000@gmail.com>
2026-02-26 17:53:36 +0200

    ts

--- script.ts
+++ script.ts
@@ -1,51 +1,71 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const serchButt = document.getElementById('serch');
-const serchInput = document.getElementById('serchInput');
-let serch;
-let main = document.querySelector('main');
+const serchButt = document.getElementById('serch') as HTMLElement;
+const serchInput = document.getElementById('serchInput') as HTMLInputElement ;
+let serch: string;
+let main = document.querySelector('main') as HTMLElement;
 console.log(main);
-async function gitTheWeather(city) {
-    try {
+
+interface WeatherData {
+    main: {
+        temp: number;
+    };
+    weather: {
+        description: string;
+    }[];
+    rain?: {
+        "1h"?: number;
+    };
+}
+
+
+async function gitTheWeather(city: string) {
+    try{
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=eba9ebd6ff1378f16fbd02e0dcfe8714`);
+
         if (response.ok) {
-            let data = await response.json();
+            let data: WeatherData = await response.json();
             console.log(response);
             console.log(data);
-            main.classList.add('d-block');
-            main.classList.remove('d-none');
-            let condition = data.weather[0].description;
+
+            main.classList.add('d-block')
+            main.classList.remove('d-none')
+            // let condition = data.weather[0].description;
+            let condition = data.weather?.[0]?.description ?? "No description";
             let tempCelsius = Math.floor(data.main.temp - 273.15);
             let rainVolume = data.rain ? data.rain["1h"] : 0;
+
             console.log(condition);
-            main.innerHTML = `
+            main.innerHTML=`
                 <p class="cityName display-1">${serch}</p>
                 <p class="temperature">${tempCelsius}°C</p>
                 <p class="Rainfall">Rainfall:${rainVolume}%</p>
                 <p class="condition h4">${condition}</p>
                 <p class="emoji display-2">☀️</p>
             `;
-        }
-        else {
+            
+        }else{
             throw new Error('could not get it');
         }
     }
-    catch (error) {
+    catch(error){
         console.error(error);
         console.error('can not');
-        main.classList.add('d-block');
-        main.classList.remove('d-none');
-        main.innerHTML = `
+        
+            main.classList.add('d-block')
+            main.classList.remove('d-none')
+
+            main.innerHTML=`
                 <h1>Wrong name</h1>
             `;
+
     }
-}
-;
+    
+};
 // gitTheWeather('ciro');
-serchButt.addEventListener('click', () => {
+
+serchButt.addEventListener('click',()=>{
     serch = serchInput.value;
     serchInput.value = '';
     console.log(serch);
     gitTheWeather(serch);
 });
-//# sourceMappingURL=script.js.map
+
